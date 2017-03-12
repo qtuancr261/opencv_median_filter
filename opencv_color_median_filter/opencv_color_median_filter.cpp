039869a90128e431d309e05fe57e489cc913bb5b
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-03-12 10:33:52 +0700

    Median filter Grayscale-Color

--- opencv_color_median_filter/opencv_color_median_filter.cpp
+++ opencv_color_median_filter/opencv_color_median_filter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <opencv2/opencv.hpp>
+#include <vector>
 using namespace std;
 using namespace cv;
 void medianFilter(Mat& input, Mat& output, int kernelSize)
